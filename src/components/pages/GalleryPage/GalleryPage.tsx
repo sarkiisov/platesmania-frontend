5dea6bd74d6d621166a03f818757ce7255aee292
sarkiisov <sarkisov1337@gmail.com>
2025-03-24 20:51:08 +0300

    chore: cards empty fallback

--- src/components/pages/GalleryPage/GalleryPage.tsx
+++ src/components/pages/GalleryPage/GalleryPage.tsx
@@ -6,12 +6,18 @@ export const GalleryPage = () => {
   const cards = useLoaderData() as Awaited<ReturnType<typeof galleyPageLoader>>
 
   return (
-    <div className="grid grid-cols-2 gap-4">
-      {cards.map((card) => (
-        <Link key={card.id} to={`/gallery/${card.id}/edit`}>
-          <Card key={card.id} {...card} />
-        </Link>
-      ))}
-    </div>
+    <>
+      {cards.length ? (
+        <div className="grid grid-cols-2 gap-4">
+          {cards.map((card) => (
+            <Link key={card.id} to={`/gallery/${card.id}/edit`}>
+              <Card key={card.id} {...card} />
+            </Link>
+          ))}
+        </div>
+      ) : (
+        <span className="my-8 text-center text-sm text-gray-500">Ничего не найдено</span>
+      )}
+    </>
   )
 }
